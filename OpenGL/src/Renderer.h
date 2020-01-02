1d3d38cea7ad845b508c5c4637e92796cd5b8654
Dan <dsinger.sax@gmail.com>
2020-01-02 15:29:08 -0500

    Add basic renderer

--- OpenGL/src/Renderer.h
+++ OpenGL/src/Renderer.h
@@ -2,6 +2,9 @@
 
 #include <GL/glew.h>
 #include <iostream>
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
 
 #define ASSERT(x) if (!(x)) __debugbreak();
 // Use this to call any OpenGL function with and get error reporting.
@@ -11,3 +14,10 @@
 
 void GLClearError();
 bool GLLogCall(const char* function, const char* file, int line);
+
+class Renderer
+{
+public:
+	void Clear() const;
+	void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+};
