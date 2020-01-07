58ff46bed5421a2e401aba6651448e5867502e92
Dan <dsinger.sax@gmail.com>
2020-01-07 16:55:36 -0500

    Remove unused OpenGL wrapper classes

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -5,11 +5,7 @@
 #include <string>
 #include <sstream>
 
-#include "VertexBuffer.h"
-#include "IndexBuffer.h"
-#include "VertexArray.h"
 #include "Shader.h"
-#include "VertexBufferLayout.h"
 #include "Model.h"
 
 #include <glm/glm.hpp>
